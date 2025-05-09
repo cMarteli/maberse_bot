59232991bc7b8c7b3a6b2e45c8a7d8faed71d0d5
CMarteli <19598552@student.curtin.edu.au>
2025-05-09 22:09:20 +0800

    start rework on bot

--- main.py
+++ main.py
@@ -3,68 +3,81 @@
 # Author: Glitch
 # Modified March, 29, 2023
 
-from webserver import keep_alive #needed for replit hosting
+from dotenv import load_dotenv
+from webserver import keep_alive  # needed for replit hosting
 
 import os
 import discord
 import responses
 
-TOKEN = os.environ['BOT_TOKEN'] #secret token is stored in replit
-WELCOME_MESSAGE = "Howdy, howdy!" #message that will be displayed when a new user joins server
+load_dotenv()  # Load environment variables from .env
 
-GREET_CHANNEL_ID = 432542755785408515 # this is the id of the "generalis" channel
+TOKEN = os.getenv('BOT_TOKEN')
 
-#allows bot to send messages
+# TOKEN = os.environ['BOT_TOKEN'] #secret token is stored in replit
+# message that will be displayed when a new user joins server
+WELCOME_MESSAGE = "Howdy, howdy!"
+
+# this is the id of the "generalis" channel
+GREET_CHANNEL_ID = 432542755785408515
+
+# allows bot to send messages
+
+
 async def send_message(message, user_message, is_private):
-  try:
-    response = await responses.handle_response(user_message)
-    await message.author.send(
-      response) if is_private else await message.channel.send(response)
+    try:
+        response = await responses.handle_response(user_message)
+        await message.author.send(
+            response) if is_private else await message.channel.send(response)
 
-  except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
 
 # @TODO: required by discord, intents.all() might be a bad practice but the bot only has limited permissions
 # need o verify how these differ from the permissions given from the bot portal
+
+
 def run_discord_bot():
-  intents = discord.Intents.all()
-  intents.members = True
-  client = discord.Client(intents=intents)
-#event that triggers when a new person joins the server
-  @client.event
-  async def on_member_join(member):
-    # Greets a new member joining the server
-    channel = client.get_channel(GREET_CHANNEL_ID)
-    await channel.send(f"{WELCOME_MESSAGE} {member.mention}")
-#debug event, lets you know when bot is launched successfully
-  @client.event
-  async def on_ready():
-    print(f'{client.user} is now running!')
-#event to let the bot read user messages on channel so it can reply if appropiate
-  @client.event
-  async def on_message(message):
-    if message.author == client.user:
-      return
+    intents = discord.Intents.all()
+    intents.members = True
+    client = discord.Client(intents=intents)
+# event that triggers when a new person joins the server
 
-    username = str(message.author)
-    user_message = str(message.content.strip())
-    channel = str(message.channel)
+    @client.event
+    async def on_member_join(member):
+        # Greets a new member joining the server
+        channel = client.get_channel(GREET_CHANNEL_ID)
+        await channel.send(f"{WELCOME_MESSAGE} {member.mention}")
+# debug event, lets you know when bot is launched successfully
 
-    print(f"{username} said: '{user_message}' ({channel})")
-#if a user message starts with ? the bot will send it's reply as a DM
-    if user_message.startswith('?'):
-      user_message = user_message[1:]
-      await send_message(message, user_message, is_private=True)
-    else:
-      await send_message(message, user_message, is_private=False)
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+# event to let the bot read user messages on channel so it can reply if appropiate
 
-  client.run(TOKEN)
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content.strip())
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+# if a user message starts with ? the bot will send it's reply as a DM
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
 
 
-#used to keep bot alive on repl
+# used to keep bot alive on repl
 
 keep_alive()
 
-TOKEN = os.environ['BOT_TOKEN']
 
 run_discord_bot()
--- responses.py
+++ responses.py
@@ -6,55 +6,55 @@ import myjokes
 
 # Rolls a die with maxVal sides
 def diceRoll(maxVal):
-  return str(random.randint(1, maxVal))
+    return str(random.randint(1, maxVal))
 
 
 async def handle_response(message) -> str:
-  p_message = message.lower()
+    p_message = message.lower()
 
-  if p_message.startswith('!help'):
-    return "Hey mate, I'm Maberse (Bot). I can roll dice, tell jokes, and tell you the weather. Try typing !roll 2*d10 to roll two 10-sided dice, type \'joke\' to hear a joke. If you want to know the weather, just ask me!"
+    if p_message.startswith('!help'):
+        return "Hey mate, I'm Maberse (Bot). I can roll dice, tell jokes, and tell you the weather. Try typing !roll 2*d10 to roll two 10-sided dice, type \'joke\' to hear a joke. If you want to know the weather, just ask me!"
 
-  if p_message.startswith('!roll'):
-    _, roll_string = p_message.split(' ', 1)
-    if '*' in roll_string:
-      # Split the roll string by the * symbol to get the number of rolls and the type of dice
-      num_rolls, dice_type = roll_string.split('*')
-      num_rolls = int(num_rolls.strip())
-    else:
-      # If no dice type is specified, assume d6
-      num_rolls = 1
-      if len(roll_string.strip()) == 0:
-        dice_type = 'd6'
-      else:
-        dice_type = roll_string.strip()
+    if p_message.startswith('!roll'):
+        _, roll_string = p_message.split(' ', 1)
+        if '*' in roll_string:
+            # Split the roll string by the * symbol to get the number of rolls and the type of dice
+            num_rolls, dice_type = roll_string.split('*')
+            num_rolls = int(num_rolls.strip())
+        else:
+            # If no dice type is specified, assume d6
+            num_rolls = 1
+            if len(roll_string.strip()) == 0:
+                dice_type = 'd6'
+            else:
+                dice_type = roll_string.strip()
 
-    # Check which dice to roll
-    if 'd10' in dice_type:
-      rolls = [diceRoll(10) for _ in range(num_rolls)]
-      return ', '.join(str(r) for r in rolls)
-    elif 'd20' in dice_type:
-      rolls = [diceRoll(20) for _ in range(num_rolls)]
-      return ', '.join(str(r) for r in rolls)
-    else:
-      rolls = [diceRoll(6) for _ in range(num_rolls)]
-      return ', '.join(str(r) for r in rolls)
+        # Check which dice to roll
+        if 'd10' in dice_type:
+            rolls = [diceRoll(10) for _ in range(num_rolls)]
+            return ', '.join(str(r) for r in rolls)
+        elif 'd20' in dice_type:
+            rolls = [diceRoll(20) for _ in range(num_rolls)]
+            return ', '.join(str(r) for r in rolls)
+        else:
+            rolls = [diceRoll(6) for _ in range(num_rolls)]
+            return ', '.join(str(r) for r in rolls)
 
-  if 'how are you' in p_message:
-    return "I'm good, thanks for asking!"
+    if 'how are you' in p_message:
+        return "I'm good, thanks for asking!"
 
-  if 'thanks mark' in p_message:
-    return "That's okay, always happy to help out a mate!"
+    if 'thanks mark' in p_message:
+        return "That's okay, always happy to help out a mate!"
 
-  if 'joke' in p_message:
-    joke = await myjokes.tell_joke()
-    return "Sure mate, here's a good one:\n" + joke
+    if 'joke' in p_message:
+        joke = await myjokes.tell_joke()
+        return "Yeah, I know a joke actually: \n" + joke
 
-  if 'legal advice' in p_message:
-    return "Sorry, I'm not programmed to give legal advice. Please consult Maberse(Real)."
+    if 'legal advice' in p_message:
+        return "Sorry, I'm not programmed to give legal advice. Please consult Maberse(Real)."
 
-  if 'weather' in p_message:
-    temperature = await weather.getweather()
-    place = weather.getLocation()
-    return "It's currently " + str(
-      temperature) + " degrees in " + place + ", mate."
+    if 'weather' in p_message:
+        temperature = await weather.getweather()
+        place = weather.getLocation()
+        return "It's currently " + str(
+            temperature) + " degrees in " + place + ", mate."
